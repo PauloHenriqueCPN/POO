68fb9165323f1aafcec63fa572b801456d5dca9e
PAULO HENRIQUE COSTA PEREIRA NUNES <202303378785@alunos.ibmec.edu.br>
2023-08-11 12:06:15 -0300

    p

---
+++ AC1/src/App.java
@@ -0,0 +1,83 @@
+public class App {        
+
+    public static void main(String[] args) throws Exception{
+        primeiro(8,5,6);
+        segundo('n');
+        terceiro(18);
+        quarto(3);
+    }
+    
+    public static void primeiro(double a, double b, double c) {
+
+        System.out.println((a + b) * 0.4 + (c * 0.2));
+
+    }   
+
+    public static void segundo(int n) {
+        n = 150;
+
+        while (n < 201) {
+            System.out.print(n++);
+            System.out.print(" ");
+        }
+        
+    }
+
+    public static void terceiro(int num) {
+        System.out.println();
+        boolean ePrimo = true;
+        String divisores = "";
+
+        if (num <= 1) {
+            ePrimo = false;
+        } else {
+            for (int i = 2; i <= (num); i++) {
+                if (num % i == 0) {
+                    ePrimo = false;
+                    divisores += i + " ";
+                }
+            }
+        }
+
+        if (ePrimo) {
+            System.out.println(num + " é um número primo.");
+        } else {
+            System.out.println(num + " não é um número primo. Ele é divisível por: " + divisores);
+        }
+    }
+
+    public static void quarto(int num) {
+
+        
+        switch (num) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+
+    }
+
+    public static void quinto() {}
+
+
+}
